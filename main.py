d68490e341897eaf713e9944f1dacc42a8f9843c
Autumn Pan <pan662607@gmail.com>
2025-03-25 19:55:20 -0400

    add claw functionslity

--- main.py
+++ main.py
@@ -1,3 +1,39 @@
+#region VEXcode Generated Robot Configuration
+from vex import *
+import urandom
+
+# Brain should be defined by default
+brain=Brain()
+
+# Robot configuration code
+
+
+# wait for rotation sensor to fully initialize
+wait(30, MSEC)
+
+
+# Make random actually random
+def initializeRandomSeed():
+    wait(100, MSEC)
+    random = brain.battery.voltage(MV) + brain.battery.current(CurrentUnits.AMP) * 100 + brain.timer.system_high_res()
+    urandom.seed(int(random))
+      
+# Set random seed 
+initializeRandomSeed()
+
+
+def play_vexcode_sound(sound_name):
+    # Helper to make playing sounds from the V5 in VEXcode easier and
+    # keeps the code cleaner by making it clear what is happening.
+    print("VEXPlaySound:" + sound_name)
+    wait(5, MSEC)
+
+# add a small delay to make sure we don't print in the middle of the REPL header
+wait(200, MSEC)
+# clear the console to make sure we don't have the REPL in the console
+print("\033[2J")
+
+#endregion VEXcode Generated Robot Configuration
 # ---------------------------------------------------------------------------- #
 #                                                                              #
 # 	Module:       main.py                                                      #
@@ -32,26 +68,44 @@ x_limit = False
 y_limit = False
 
 # Define motors
-x_motor_1 = Motor(Ports.PORT1)
-x_motor_2 = Motor(Ports.PORT2)
+x_motor_1 = Motor(Ports.PORT2)
+x_motor_2 = Motor(Ports.PORT13)
 
-y_motor = Motor(Ports.PORT3)
+y_motor = Motor(Ports.PORT12)
 
+claw_rotation_motor = Motor(Ports.PORT11)
+claw_depression_motor = Motor(Ports.PORT3)
+
+def set_claw_depression():
+    if controller.buttonR1.pressing():
+        claw_depression_motor.spin(REVERSE, 50)
+    elif controller.buttonR2.pressing():
+        claw_depression_motor.spin(FORWARD, 50)
+    else:
+        claw_depression_motor.stop()
+
+def set_claw_rotation():
+    if controller.axis1.position() > 10:
+        claw_rotation_motor.spin(FORWARD, 50)
+    elif controller.axis1.position() < -10:
+        claw_rotation_motor.spin(REVERSE, 50)
+    else:
+        claw_rotation_motor.stop()
 # Sets the direction of motion
 def set_claw_dir():
     global x_dir, y_dir
     if(abs(controller.axis4.position()) > 5):
-        x_dir = controller.axis4.position()/abs(controller.axis4.position())
+        y_dir = controller.axis4.position()/abs(controller.axis4.position())
     if(abs(controller.axis3.position()) > 5):
-        y_dir = controller.axis3.position()/abs(controller.axis3.position())
+        x_dir = -controller.axis3.position()/abs(controller.axis3.position())
 
 # Sets the speed of the claw motors
 def set_claw_speed():
     global x_speed, y_speed, exponent
 
     # Speed should be raised to some power for better control
-    x_speed = abs(controller.axis4.position()**exponent)
-    y_speed = abs(controller.axis3.position()**exponent)
+    y_speed = abs(controller.axis4.position()**exponent)
+    x_speed = abs(controller.axis3.position()**exponent)
 
     # Speed should not exceed 100
     if x_speed > 100:
@@ -65,7 +119,7 @@ def move_motor():
     global x_speed, y_speed, x_dir, y_dir, x_limit, y_limit
 
     if x_speed > 5 and not x_limit:
-        x_motor_1.spin(FORWARD, x_speed*x_dir)
+        x_motor_1.spin(REVERSE, x_speed*x_dir)
         x_motor_2.spin(FORWARD, x_speed*x_dir)
     else:
         x_motor_1.stop()
@@ -96,7 +150,9 @@ def main():
         set_claw_dir()
         set_claw_speed()
         move_motor()
-        limit()
+        set_claw_depression()
+        set_claw_rotation()
+        # limit()
         wait(10, MSEC)
 
 main()
